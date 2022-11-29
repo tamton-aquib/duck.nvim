7159cdc240b5e6e5131daff267764ca3e28d3dfa
Aquib <aquibjavedt007@gmail.com>
2022-11-30 00:05:09 +0530

    clear up indentation

--- lua/duck.lua
+++ lua/duck.lua
@@ -4,58 +4,58 @@ local conf = {character="🦆", speed=10, width=2, height=1}
 
 -- TODO: a mode to wreck the current buffer?
 local waddle = function(duck, speed)
-	local timer = vim.loop.new_timer()
-	local new_duck = { name = duck, timer = timer }
-	table.insert(M.ducks_list, new_duck)
+    local timer = vim.loop.new_timer()
+    local new_duck = { name = duck, timer = timer }
+    table.insert(M.ducks_list, new_duck)
 
-	local waddle_period = 1000 / (speed or conf.speed)
-	vim.loop.timer_start(timer, 1000, waddle_period, vim.schedule_wrap(function()
-		if vim.api.nvim_win_is_valid(duck) then
-			local config = vim.api.nvim_win_get_config(duck)
-			local col, row = config["col"][false], config["row"][false]
+    local waddle_period = 1000 / (speed or conf.speed)
+    vim.loop.timer_start(timer, 1000, waddle_period, vim.schedule_wrap(function()
+        if vim.api.nvim_win_is_valid(duck) then
+            local config = vim.api.nvim_win_get_config(duck)
+            local col, row = config["col"][false], config["row"][false]
 
-			math.randomseed(os.time()*duck)
-			local movement = math.ceil(math.random()*4)
-			if movement == 1 or row <= 0 then
-				config["row"] = row + 1
-			elseif movement == 2 or row >= vim.o.lines-1 then
-				config["row"] = row - 1
-			elseif movement == 3 or col <= 0 then
-				config["col"] = col + 1
-			elseif movement == 4 or col >= vim.o.columns-2 then
-				config["col"] = col - 1
-			end
-			vim.api.nvim_win_set_config(duck, config)
-		end
-	end))
+            math.randomseed(os.time()*duck)
+            local movement = math.ceil(math.random()*4)
+            if movement == 1 or row <= 0 then
+                config["row"] = row + 1
+            elseif movement == 2 or row >= vim.o.lines-1 then
+                config["row"] = row - 1
+            elseif movement == 3 or col <= 0 then
+                config["col"] = col + 1
+            elseif movement == 4 or col >= vim.o.columns-2 then
+                config["col"] = col - 1
+            end
+            vim.api.nvim_win_set_config(duck, config)
+        end
+    end))
 end
 
 M.hatch = function(character, speed)
-	local buf = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_buf_set_lines(buf , 0, 1, true , {character or conf.character})
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(buf , 0, 1, true , {character or conf.character})
 
-	local duck = vim.api.nvim_open_win(buf, false, {
-		relative='cursor', style='minimal', row=1, col=1, width=conf.width, height=conf.height
-	})
-	vim.api.nvim_win_set_option(duck, 'winhighlight', 'Normal:Normal')
+    local duck = vim.api.nvim_open_win(buf, false, {
+        relative='cursor', style='minimal', row=1, col=1, width=conf.width, height=conf.height
+    })
+    vim.api.nvim_win_set_option(duck, 'winhighlight', 'Normal:Normal')
 
-	waddle(duck, speed)
+    waddle(duck, speed)
 end
 
 M.cook = function()
-	local last_duck = M.ducks_list[#M.ducks_list]
+    local last_duck = M.ducks_list[#M.ducks_list]
 
     if not last_duck then
         vim.notify("No ducks to cook!")
         return
     end
 
-	local duck = last_duck['name']
-	local timer = last_duck['timer']
-	table.remove(M.ducks_list, #M.ducks_list)
-	timer:stop()
+    local duck = last_duck['name']
+    local timer = last_duck['timer']
+    table.remove(M.ducks_list, #M.ducks_list)
+    timer:stop()
 
-	vim.api.nvim_win_close(duck, true)
+    vim.api.nvim_win_close(duck, true)
 end
 
 M.setup = function(opts)
