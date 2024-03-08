e26202e7aeb6a510169680c86be9ae1e793bccf2
Mathieu Scolas <worming@hotmail.be>
2024-03-08 14:21:45 +0100

    remove gards that is now at top of the stack

--- lua/waddle.lua
+++ lua/waddle.lua
@@ -6,11 +6,6 @@ M.waddle = function(buf, speed, conf, strategy)
     local new_duck = { name = buf, timer = timer }
     table.insert(M.ducks_list, new_duck)
 
-    if strategy == nil then
-        local rnd_wandle = require('random_waddle')
-        strategy = rnd_wandle.random_waddle(rnd_wandle)
-    end
-
     local waddle_period = 1000 / (speed or conf.speed)
     vim.loop.timer_start(timer, 1000, waddle_period, vim.schedule_wrap(function()
         if vim.api.nvim_win_is_valid(buf) then
@@ -18,7 +13,7 @@ M.waddle = function(buf, speed, conf, strategy)
             local col, row = 0, 0
             if vim.version().minor < 10 then -- Neovim 0.9
                 col, row = config["col"][false], config["row"][false]
-            else                             -- Neovim 0.10
+            else                       -- Neovim 0.10
                 col, row = config["col"], config["row"]
             end
 
